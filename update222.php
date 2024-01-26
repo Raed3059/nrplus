c8b60790aa28c65a8c5837f723d1786bec41924a
Raed3059 <109338858+Raed3059@users.noreply.github.com>
2024-01-26 13:25:11 +0300

    Delete update222.php

--- update222.php
+++
@@ -1,8 +0,0 @@
-<?php
-system("cd /www/wwwroot/apt.nrplus.app");
-system("dpkg-scanpackages -m ./debs /dev/null > Packages");
-system("bzip2 -c Packages > Packages.bz2");
-system("gzip  -c Packages > Packages.gz");
-
-echo "NRPLUS YYDS";
-?>
